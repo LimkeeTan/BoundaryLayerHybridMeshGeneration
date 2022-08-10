00ec89624517a3fe7109db43215c53299d14e9b3
LimkeeTan <50545690+LimkeeTan@users.noreply.github.com>
2022-08-10 09:25:45 +0800

    Update vertex_normal.h

--- src/generate_mesh/vertex_normal.h
+++ src/generate_mesh/vertex_normal.h
@@ -1,6 +1,7 @@
 #ifndef VERTEX_NORMAL_H_
 #define VERTEX_NORMAL_H_
 #include "../mesh_utils.h"
+#include "../osqp_eigen/OsqpEigen.h"
 
 namespace vertex_normal {
 	int computeVertexNormal(const global_type::Mesh& mesh,
