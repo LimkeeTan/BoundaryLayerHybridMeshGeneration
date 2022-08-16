2787994a9cda2a7ddbfd09f5f870751338de64a9
LimkeeTan <50545690+LimkeeTan@users.noreply.github.com>
2022-08-16 18:47:56 +0800

    Update topo_hybrid.cpp

--- src/generate_mesh/topo_hybrid.cpp
+++ src/generate_mesh/topo_hybrid.cpp
@@ -1,24 +1,15 @@
 #include "topo_hybrid.h"
 
 namespace topo_hybrid {
-	void computeTriNormal(const std::vector< std::vector < double > >& vertices,
-		std::vector < double >& triNormal
+	void computeTriNormal(const std::vector< Eigen::Vector3d >& vertices,
+		Eigen::Vector3d& triNormal
 	)
 	{
-		std::vector < Eigen::Vector3d > V(vertices.size());
 		Eigen::Vector3d E0;
 		Eigen::Vector3d E1;
-		Eigen::Vector3d Normal;
-		for (int i = 0; i < vertices.size(); ++i) {
-			for (int j = 0; j < 3; ++j) {
-				V[i][j] = vertices[i][j];
-			}
-		}
-		E0 = V[1] - V[0]; E1 = V[2] - V[0];
-		Normal = E0.cross(E1).normalized();
-		for (int i = 0; i < 3; ++i) {
-			triNormal[i] = Normal[i];
-		}
+		E0 = vertices[1] - vertices[0];
+		E1 = vertices[2] - vertices[0];
+		triNormal = E0.cross(E1).normalized();
 	}
 
 	int validTetCheck(const global_type::Mesh& tetTopo,
@@ -32,11 +23,19 @@ namespace topo_hybrid {
 		mesh_utils::constructVerCellMap(tetTopo.vecCells, verCellMap);
 		std::vector < size_t > relateCell;
 		std::vector < size_t > singleCell;
-		std::vector < double > triNormal(3);
-		std::vector < std::vector < double > > threeVer(3);
-		int triFace[3];
+		Eigen::Vector3d triNormal;
+		Eigen::Vector3d queryVer;
+		std::vector < Eigen::Vector3d > threeVer(3);
+		int triFace[3]{};
 		int idx;
+		double distance;
 		for (size_t i = 0; i < vertMap.size(); ++i) {
+			if (vertMap.at(i).size() == 1) {
+				continue;
+			}
+			for (int j = 0; j < 3; ++j) {
+				queryVer[j] = prismTopo.vecVertices[vertMap.at(i).back()][j];
+			}
 			relateCell = verCellMap[i];
 			for (int j = 0; j < relateCell.size(); ++j) {
 				singleCell = tetTopo.vecCells[relateCell[j]];
@@ -46,37 +45,25 @@ namespace topo_hybrid {
 						break;
 					}
 				}
-				switch (idx) {
-				case 0:
-					for (int k = 0; k < 3; ++k) {
-						triFace[k] = global_type::tetFaces[2][k];
-						threeVer[k] = tetTopo.vecVertices[singleCell[triFace[k]]];
+				for (int k = 0; k < 3; ++k) {
+					triFace[k] = global_type::tetFaces[global_type::tetVerOppositeFace[idx]][k];
+					if (vertMap.count(singleCell[triFace[k]]) && vertMap.at(singleCell[triFace[k]]).size() > 1) {
+						for (int p = 0; p < 3; ++p) {
+							threeVer[k][p] = prismTopo.vecVertices[vertMap.at(singleCell[triFace[k]]).back()][p];
+						}
 					}
-					break;
-				case 1:
-					for (int k = 0; k < 3; ++k) {
-						triFace[k] = global_type::tetFaces[1][k];
-						threeVer[k] = tetTopo.vecVertices[singleCell[triFace[k]]];
+					else {
+						for (int p = 0; p < 3; ++p) {
+							threeVer[k][p] = tetTopo.vecVertices[singleCell[triFace[k]]][p];
+						}
 					}
-					break;
-				case 2:
-					for (int k = 0; k < 3; ++k) {
-						triFace[k] = global_type::tetFaces[3][k];
-						threeVer[k] = tetTopo.vecVertices[singleCell[triFace[k]]];
-					}
-					break;
-				case 3:
-					for (int k = 0; k < 3; ++k) {
-						triFace[k] = global_type::tetFaces[0][k];
-						threeVer[k] = tetTopo.vecVertices[singleCell[triFace[k]]];
-					}
-					break;
-				default:
-					std::cout << "unknow index of tet" << std::endl;
-					return 0;
-					break;
 				}
 				computeTriNormal(threeVer, triNormal);
+				distance = (triNormal * (queryVer - threeVer[0]).dot(triNormal)).norm();
+				while (distance < 0.1) {
+					std::cout << "flatten tet, should reduce marching distance" << std::endl;
+					std::cout << relateCell[j] << " " << distance << std::endl;
+				}
 			}
 		}
 		return 1;
@@ -96,7 +83,7 @@ namespace topo_hybrid {
 			return 0;
 		}
 
-		// combine to generate hybridMesh
+		// combine to obtain hybridMesh
 		size_t triVerNum = hybridMesh.vecVertices.size();
 		hybridMesh.vecVertices = prismTopo.vecVertices;
 		hybridMesh.vecCells = prismTopo.vecCells;
